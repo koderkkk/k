09b6104abe480d3791565bfb939c6eb6d0a6a39d
koderkkk <410307232@qq.com>
2018-08-28 16:36:12 +0800

    Drawable

--- app/src/main/java/com/cnbeta/fk/kewei/k/MainActivity.java
+++ app/src/main/java/com/cnbeta/fk/kewei/k/MainActivity.java
@@ -1,13 +1,28 @@
 package com.cnbeta.fk.kewei.k;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseIntArray;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public static final int PAGE_HOME = 0;
+    public static final int PAGE_HOT_ARTICLES = 1;
+    public static final int PAGE_HOT_COMMENTS = 2;
+
+    //SparseIntArray取代了HashMap
+    private static SparseIntArray INDEX_ID_MAPPING = new SparseIntArray();
+
+    static{
+//        INDEX_ID_MAPPING.put();//TODO
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
