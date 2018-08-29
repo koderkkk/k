1b4a47245f1531ee6911b0accd71a516fa12d9da
koderkkk <410307232@qq.com>
2018-08-29 08:32:56 +0800

    29morning

--- app/src/main/java/com/cnbeta/fk/kewei/k/MainActivity.java
+++ app/src/main/java/com/cnbeta/fk/kewei/k/MainActivity.java
@@ -1,8 +1,16 @@
 package com.cnbeta.fk.kewei.k;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.SparseIntArray;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +22,33 @@ public class MainActivity extends AppCompatActivity {
     //SparseIntArray取代了HashMap
     private static SparseIntArray INDEX_ID_MAPPING = new SparseIntArray();
 
-    static{
+    static {
 //        INDEX_ID_MAPPING.put();//TODO
     }
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.appbar)
+    AppBarLayout mAppbar;
+    @BindView(R.id.pager)
+    ViewPager mPager;
+    @BindView(R.id.shadow_container)
+    FrameLayout mShadowContainer;
+    @BindView(R.id.bottom_bar)
+    BottomNavigationView mBottomBar;
+
+    //初始化各个控件
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
 
     }
 
